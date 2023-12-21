bbe36fe77205b996953e46d3aaa37ec743b5ce3a
jula <122871245+jmahanera@users.noreply.github.com>
2023-12-21 15:30:26 +0100

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -12,7 +12,7 @@ const apiUrl = 'https://primemovies-39075872fbeb.herokuapp.com/';
 @Injectable({
   providedIn: 'root',
 })
-export class FetchApiDataService {
+export class UserRegistrationService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {}
