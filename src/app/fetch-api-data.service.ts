bf4d5bf4ebef024aa185e30313c6f39b798f42f9
jula <122871245+jmahanera@users.noreply.github.com>
2023-12-22 13:51:42 +0100

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -51,9 +51,6 @@ export class FetchApiDataService {
   }
 }
 
-@Injectable({
-  providedIn: 'root',
-})
 export class UserRegistrationService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
