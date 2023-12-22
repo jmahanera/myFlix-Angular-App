edc78740069588e01d2ed07b1d953c4b7a8bf59b
jula <122871245+jmahanera@users.noreply.github.com>
2023-12-22 12:54:26 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  // Add other routes if needed
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
