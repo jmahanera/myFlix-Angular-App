593f6292719d48fe649862d36e01f402c21a1ae8
jula <122871245+jmahanera@users.noreply.github.com>
2023-12-23 22:30:09 +0100

    Update welcome-page.component.ts

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  templateUrl: '../app.component.html',
+  styleUrls: ['../app.component.scss'],
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
