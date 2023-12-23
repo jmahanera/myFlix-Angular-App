601a982cd635084c26fc05e291dc79ed678cedba
jula <122871245+jmahanera@users.noreply.github.com>
2023-12-23 17:24:40 +0100

    Update movie-card.component.ts

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetAllMoviesService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.scss'
+  styleUrls: ['./movie-card.component.scss'], // Corrected from 'styleUrl' to 'styleUrls'
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
+  movies: any[] = [];
 
+  constructor(public fetchMovies: GetAllMoviesService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.fetchMovies.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      // return this.movies; // No need to return anything here
+    });
+  }
 }
