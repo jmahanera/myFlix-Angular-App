eeac8d920991e98ca21b3f6bded9d4675a579c21
jula <122871245+jmahanera@users.noreply.github.com>
2023-12-23 13:26:44 +0100

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,11 +1,7 @@
+// fetch-api-data.service.ts
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import {
-  HttpClient,
-  HttpHeaders,
-  HttpErrorResponse,
-} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 
 const apiUrl = 'https://primemovies-39075872fbeb.herokuapp.com/';
@@ -13,19 +9,8 @@ const apiUrl = 'https://primemovies-39075872fbeb.herokuapp.com/';
 @Injectable({
   providedIn: 'root',
 })
-export class FetchApiDataService {
-  constructor(private http: HttpClient) {}
-
-  public userLogin(userDetails: any): Observable<any> {
-    // Make sure 'username' and 'password' are properties of userDetails
-    const { username, password } = userDetails;
-
-    return this.http
-      .post(apiUrl + 'login', { username, password })
-      .pipe(catchError(this.handleError));
-  }
-
-  private handleError(error: HttpErrorResponse): any {
+export class ErrorHandlerService {
+  handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
       console.error('Some error occurred:', error.error.message);
     } else {
@@ -40,23 +25,39 @@ export class FetchApiDataService {
 @Injectable({
   providedIn: 'root',
 })
+export class UserLoginService {
+  constructor(
+    private http: HttpClient,
+    private errorHandler: ErrorHandlerService
+  ) {}
+
+  public userLogin(userDetails: any): Observable<any> {
+    return this.http
+      .post(apiUrl + 'login', userDetails)
+      .pipe(
+        catchError((error: HttpErrorResponse) =>
+          this.errorHandler.handleError(error)
+        )
+      );
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
 export class UserRegistrationService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private errorHandler: ErrorHandlerService
+  ) {}
 
   public userRegistration(userDetails: any): Observable<any> {
     return this.http
       .post(apiUrl + 'users', userDetails)
-      .pipe(catchError(this.handleError));
-  }
-
-  private handleError(error: HttpErrorResponse): any {
-    if (error.error instanceof ErrorEvent) {
-      console.error('Some error occurred:', error.error.message);
-    } else {
-      console.error(
-        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
+      .pipe(
+        catchError((error: HttpErrorResponse) =>
+          this.errorHandler.handleError(error)
+        )
       );
-    }
-    return throwError('Something bad happened; please try again later.');
   }
 }
